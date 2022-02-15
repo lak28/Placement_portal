2a215841aac857a3cd05e233d9493a323894a124
lak28 <89638544+lak28@users.noreply.github.com>
2022-02-15 18:00:41 +0530

    Update batchController.js

--- controllers/batchController.js
+++ controllers/batchController.js
@@ -1,3 +1,5 @@
+// File to contain the controllers of the batches
+
 const Batch= require('../models/batch');
 
 // returning add batch form 
@@ -5,14 +7,14 @@ module.exports.add= function(req,res){
     return res.render('forms/addbatch');
 }
 
-// handling batch creation form submission and adding data in db 
+// handling a batch creation form submission and adding data in database
 module.exports.create= function(req,res){
     req.flash('success','Batch Created Successfully!');
     Batch.create(req.body);
     res.redirect('/batch/view');
 }
 
-// retriving all batches and pases that list to views 
+// retriving all batches information and passing that list to views 
 module.exports.view=async function(req,res){
     try{
         let batches= await Batch.find({});
