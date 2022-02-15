054867c53702bc0d9ee07e3e606ed13e752056be
lak28 <89638544+lak28@users.noreply.github.com>
2022-02-15 17:54:28 +0530

    Update middleware.js

--- config/middleware.js
+++ config/middleware.js
@@ -1,6 +1,4 @@
-
-
-// created middleware for flash messages
+//  middleware for messages for the user halp
 module.exports.setFlash=function(req,res,next){
     res.locals.flash = {
         'success':req.flash('success'),
